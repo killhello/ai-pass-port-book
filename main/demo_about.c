4ca280dc47f54cf9ed243b746af68f665e5c9009
killhello <ebook@folotoy.local>
2026-08-25 21:08:06 +0800

    feat(about): 使用官方开源声明全文，可滚动
    - 基于 OPEN SOURCE SOFTWARE NOTICE.txt 官方文档
    - 包含完整免责声明、6个软件许可证、Written Offer
    - 可滚动查看全部内容

--- main/demo_about.c
+++ main/demo_about.c
@@ -19,40 +19,69 @@ void demo_about_enter(void) {
     lv_obj_set_style_text_font(title, &notosanssc_16, 0);
     lv_obj_set_style_text_color(title, lv_color_hex(0xFFFFFF), 0);
     lv_label_set_text(title, "开源声明");
-    lv_obj_align(title, LV_ALIGN_TOP_MID, 0, 8);
+    lv_obj_align(title, LV_ALIGN_TOP_MID, 0, 4);
 
-    lv_obj_t *txt = lv_label_create(s_scr);
+    lv_obj_t *scroll = lv_obj_create(s_scr);
+    lv_obj_set_size(scroll, 230, 260);
+    lv_obj_align(scroll, LV_ALIGN_TOP_MID, 0, 24);
+    lv_obj_set_style_bg_color(scroll, lv_color_hex(0x000000), 0);
+    lv_obj_set_style_bg_opa(scroll, LV_OPA_COVER, 0);
+    lv_obj_set_style_border_width(scroll, 0, 0);
+    lv_obj_set_style_pad_all(scroll, 0, 0);
+    lv_obj_set_scrollbar_width(scroll, 3);
+    lv_obj_set_scrollbar_color(scroll, lv_color_hex(0x555555), 0);
+
+    lv_obj_t *txt = lv_label_create(scroll);
     lv_obj_set_style_text_font(txt, &notosanssc_16, 0);
     lv_obj_set_style_text_color(txt, lv_color_hex(0xBBBBBB), 0);
     lv_obj_set_width(txt, 220);
     lv_label_set_long_mode(txt, LV_LABEL_LONG_WRAP);
-    lv_obj_align(txt, LV_ALIGN_TOP_MID, 0, 30);
+    lv_obj_set_style_pad_all(txt, 5, 0);
     lv_label_set_text(txt,
-        "FoloToy AI Passport\n"
-        "https://github.com/killhello\n\n"
-        "本固件使用以下开源项目:\n\n"
-        "ESP-IDF (v5.5.3)\n"
-        "  Apache-2.0 License\n"
-        "  Espressif Systems\n\n"
-        "LVGL (v9.5)\n"
-        "  MIT License\n"
-        "  lvgl.io\n\n"
-        "esp_lvgl_port\n"
-        "  Apache-2.0 License\n"
-        "  Espressif Systems\n\n"
-        "esp_codec_dev\n"
-        "  Apache-2.0 License\n"
-        "  Espressif Systems\n\n"
-        "esp_button\n"
-        "  Apache-2.0 License\n"
-        "  Espressif Systems"
+        "OPEN SOURCE SOFTWARE NOTICE\n\n"
+        "The open source software licenses are\n"
+        "granted by the respective right holders.\n"
+        "This notice is provided on behalf of\n"
+        "the manufacturer.\n\n"
+        "--- Warranty Disclaimer ---\n\n"
+        "THE OPEN SOURCE SOFTWARE IN THIS\n"
+        "PRODUCT IS DISTRIBUTED IN THE HOPE\n"
+        "THAT IT WILL BE USEFUL, BUT WITHOUT\n"
+        "ANY WARRANTY.\n\n"
+        "--- Software List ---\n\n"
+        "1. ai-passport\n"
+        "   Copyright (c) 2026 BingYan\n"
+        "   License: MIT\n\n"
+        "2. ESP-IDF v5.5.3\n"
+        "   Copyright (c) Espressif Systems\n"
+        "   License: Apache-2.0\n\n"
+        "3. LVGL v9.5.0\n"
+        "   Copyright (c) 2022 LVGL LLC\n"
+        "   License: MIT\n\n"
+        "4. esp_lvgl_port\n"
+        "   Copyright (c) Espressif Systems\n"
+        "   License: Apache-2.0\n\n"
+        "5. esp_codec_dev\n"
+        "   Copyright (c) Espressif Systems\n"
+        "   License: Apache-2.0\n\n"
+        "6. esp_button\n"
+        "   Copyright (c) Espressif Systems\n"
+        "   License: Apache-2.0\n\n"
+        "--- Written Offer ---\n\n"
+        "We will provide the complete source\n"
+        "code licensed under open source\n"
+        "license if you send a written request\n"
+        "by email to: by@bingyan.xyz\n\n"
+        "This offer is valid for three years\n"
+        "from the moment we distributed the\n"
+        "product or firmware."
     );
 
     lv_obj_t *hint = lv_label_create(s_scr);
     lv_obj_set_style_text_font(hint, &notosanssc_16, 0);
     lv_obj_set_style_text_color(hint, lv_color_hex(0x888888), 0);
-    lv_label_set_text(hint, "长按 OK 返回");
-    lv_obj_align(hint, LV_ALIGN_BOTTOM_MID, 0, -8);
+    lv_label_set_text(hint, "上下滚动 长按OK返回");
+    lv_obj_align(hint, LV_ALIGN_BOTTOM_MID, 0, -4);
 
     lv_screen_load(s_scr);
 }
